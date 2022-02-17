4189710237fe41f9044764b3dfb6d8a20b1d0d4b
VinicinBraga <vinicius@bragamatta.com.br>
2022-02-17 15:51:37 -0300

    Create Enemies

--- index.js
+++ index.js
@@ -41,6 +41,27 @@ class Projectile {
   }
 }
 
+class Enemy {
+  constructor(x, y, radius, color, velocity) {
+    this.x = x;
+    this.y = y;
+    this.radius = radius;
+    this.color = color;
+    this.velocity = velocity;
+  }
+  draw() {
+    cr.beginPath();
+    cr.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+    cr.fillStyle = this.color;
+    cr.fill();
+  }
+  update() {
+    this.draw();
+    this.x = this.x + this.velocity.x;
+    this.y = this.y + this.velocity.y;
+  }
+}
+
 const x = canvas.width / 2;
 const y = canvas.height / 2;
 
@@ -48,12 +69,45 @@ const player = new Player(x, y, 30, "blue");
 
 const projectiles = [];
 
+const enemies = [];
+
+function spawnEnemies() {
+  setInterval(() => {
+    const radius = Math.random() * (30 - 4) + 4;
+
+    let x;
+    let y;
+
+    if (Math.random() < 0.5) {
+      x = Math.random() < 0.5 ? 0 - radius : canvas.width + radius;
+      y = Math.random() * canvas.height;
+    } else {
+      x = Math.random() * canvas.width;
+      y = Math.random() < 0.5 ? 0 - radius : canvas.height + radius;
+    }
+    const color = "green";
+
+    const angle = Math.atan2(canvas.height / 2 - y, canvas.width / 2 - x);
+
+    const velocity = {
+      x: Math.cos(angle),
+      y: Math.sin(angle),
+    };
+
+    enemies.push(new Enemy(x, y, radius, color, velocity));
+  }, 1000);
+}
+
 function animate() {
   requestAnimationFrame(animate);
   cr.clearRect(0, 0, canvas.width, canvas.height);
   player.draw();
   projectiles.forEach((projectile) => {
     projectile.update();
+  });
+
+  enemies.forEach((enemy) => {
+    enemy.update();
   });
 }
 
@@ -72,3 +126,4 @@ addEventListener("click", (event) => {
 });
 
 animate();
+spawnEnemies();
